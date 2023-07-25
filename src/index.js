0e61701e80454b6a302981c83814239773025f13
Subbu-battala <subbubattala01@gmail.com>
2023-07-25 10:12:14 +0530

    react crud

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Css and js files
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+//bootstrap icons css
+import "bootstrap-icons/font/bootstrap-icons.css";
+// toastify css
+import "react-toastify/dist/ReactToastify.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
